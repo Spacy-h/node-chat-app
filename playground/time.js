51e852b6aae9ad727597adb920b4ae593da8791b
Sparsh <sparshddcse@gmail.com>
2018-09-09 18:10:40 +0530

    Format timestamps using js

---
+++ playground/time.js
@@ -0,0 +1,18 @@
+var moment = require('moment');
+
+
+// var date = new Date();
+// var months =
+// console.log(date.getMonth());
+
+
+// var date = moment();
+// date.add(100, 'years').subtract(9, 'months');
+// console.log(date.format('MMM Do YYYY '));
+
+var someTimestamp = moment().valueOf();
+console.log(someTimestamp);
+
+var createdAt = 1234
+var time = moment(createdAt);
+console.log(time.format('h:mm a'));
--- public/js/index.js
+++ public/js/index.js
@@ -22,9 +22,9 @@ socket.on('disconnect', function() {
 //   console.log('New email', email);
 // });
 socket.on('newMessage', function(message) {
-  console.log('newMessage:', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
  });
@@ -37,10 +37,11 @@ socket.on('newMessage', function(message) {
  // });
 
 socket.on('newLocationMessage', function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My Current Location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
