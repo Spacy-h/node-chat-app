de120f516b21723676e7179bab5ce0dad353cd63
Sparsh <sparshddcse@gmail.com>
2018-09-10 20:14:15 +0530

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,8 +16,17 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function() {
-  console.log('Connected to Server');
+  // console.log('Connected to Server');
+  var params = jQuery.deparam(window.location.search);
 
+  socket.emit('join', params, function (err) {
+    if (err){
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    }
+  });
 //   socket.emit('createEmail', {
 //     to: 'jen@example.com',
 //     text: 'Hey this is sparsh'
@@ -33,6 +42,16 @@ socket.on('disconnect', function() {
   console.log('Disconnected from Server');
 });
 
+socket.on('updateUserList', function (users) {
+  //console.log('Users List', users);
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function (user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
+});
 // socket.on('newEmail', function(email) {
 //   console.log('New email', email);
 // });
---
+++ server/utils/users.js
@@ -0,0 +1,54 @@
+[{
+  id: '',
+  name: 'Sparsh',
+  room: 'The Office'
+}]
+
+
+// class Person {
+//   constructor (name, age){
+//     this.name = name;
+//     this.age = age;
+//   }
+//   getUserDescription () {
+//     return `${this.name} is ${this.age} year old`;
+//   }
+// }
+//
+// var me = new Person('Sparsh', 24);
+// var description = me.getUserDescription();
+// console.log(description);
+
+class Users {
+  constructor () {
+    this.users = [];
+  }
+  addUser (id, name, room) {
+    var user = {id, name, room};
+    this.users.push(user);
+    return user;
+  }
+  removeUser (id) {
+    var user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList (room) {
+    var users = this.users.filter((user) => {
+      return user.room === room;
+    });
+    var namesArray = users.map((user) => {
+      return user.name;
+    });
+    return namesArray;
+  }
+}
+
+module.exports = {Users};
